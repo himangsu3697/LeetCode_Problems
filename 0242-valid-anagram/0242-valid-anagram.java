250cea8107cc8725bb3296c692b572b6e645a0a0
Himangsu Sen <himangsusen18@gmail.com>
2026-07-23 12:02:59 +0530

    Time: 6 ms (43.25%), Space: 44.4 MB (67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,22 @@
-import java.util.HashMap;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) {
+        if (s.length() != t.length()) {
             return false;
         }
-        HashMap <Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++) {
-            if(map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-            } else {
-                map.put(s.charAt(i),1);
-            }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for(int i=0; i<t.length(); i++) {
-            if(map.containsKey(t.charAt(i))) {
-                if(map.get(t.charAt(i)) == 1) {
-                    map.remove(t.charAt(i));
-                } else {
-                    map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
-                }
-            } else {
+        for (int count : freq) {
+            if (count != 0) {
                 return false;
             }
         }
-        return map.isEmpty();
+
+        return true;
     }
 }
