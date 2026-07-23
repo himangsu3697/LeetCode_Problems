31fc266c5e14a802d4ce3d278a56e5defb5b0225
Himangsu Sen <himangsusen18@gmail.com>
2026-07-23 19:26:34 +0530

    Time: 1 ms (62.3%), Space: 43.2 MB (67.73%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) == last.charAt(i)) {
+                sb.append(first.charAt(i));
+            } else {
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+}
