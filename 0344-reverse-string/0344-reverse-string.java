63d1dce5b7f3df14f8f1feb7cca05c0f866b0ceb
Himangsu Sen <himangsusen18@gmail.com>
2026-07-24 20:39:42 +0530

    Time: 0 ms (100%), Space: 48.2 MB (55.62%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0, high = s.length-1;
+        char temp;
+        while(low < high) {
+            temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
