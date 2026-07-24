827ff7827f383492dc973b5e1325c6d76f71db3e
Himangsu Sen <himangsusen18@gmail.com>
2026-07-24 21:11:35 +0530

    Time: 19 ms (5.43%), Space: 53 MB (5.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,19 +1,22 @@
 class Solution {
     public String reverseWords(String s) {
-        s=s.trim() + " ";
+        s = s.trim() + " ";
         String res = "", currWord = "";
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) != ' ') {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
                 currWord += s.charAt(i);
             } else {
-                if(i != s.length() - 1 && currWord != "") {
-                    res = " " + currWord + res;
-                } else {
-                    res = currWord + res;
+                if (!currWord.isEmpty()) {
+                    if (res.isEmpty()) {
+                        res = currWord;
+                    } else {
+                        res = currWord + " " + res;
+                    }
+                    currWord = "";
                 }
-                currWord = "";
             }
         }
+
         return res;
     }
 }
