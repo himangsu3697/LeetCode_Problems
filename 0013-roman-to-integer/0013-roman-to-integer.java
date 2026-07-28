913c50291e876c719330a13a746c0a70bd172b5c
Himangsu Sen <himangsusen18@gmail.com>
2026-07-28 20:05:58 +0530

    Time: 2 ms (100%), Space: 46.2 MB (86.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,34 @@
-import java.util.HashMap;
 class Solution {
-    public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        int res = 0;
-        for(int i=0; i<s.length()-1; i++) {
-            if(map.get(s.charAt(i)) >= map.get(s.charAt(i+1))) {
-                res += map.get(s.charAt(i));
-            } else {
-                res -= map.get(s.charAt(i));
-            }
+
+    public int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
         }
-        res += map.get(s.charAt(s.length() -1));
+        return 0;
+    }
+
+    public int romanToInt(String s) {
+        int res = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = value(s.charAt(i));
+
+            if (curr < prev) {
+                res -= curr;
+            } else {
+                res += curr;
+            }
+
+            prev = curr;
+        }
+
         return res;
     }
 }
