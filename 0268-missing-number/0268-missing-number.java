a7d0320ec19ccb0e71cebd73e3b80172bbe805fd
Himangsu Sen <himangsusen18@gmail.com>
2026-07-28 19:32:22 +0530

    Time: 0 ms (100%), Space: 47.3 MB (66.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int origSum = n*(n+1)/2;
         int currSum = nums[0];
         for(int i=1; i<n; i++) {
             currSum += nums[i];
         }
-        return origSum - currSum;
+        return (n*(n+1)/2) - currSum;
     }
 }
