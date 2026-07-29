86c7b63c85782efd72df0ff6ecf17374db7c4a0e
Himangsu Sen <himangsusen18@gmail.com>
2026-07-29 19:05:54 +0530

    Time: 11 ms (7.4%), Space: 43 MB (72.96%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 class Solution {
     public int longestPalindrome(String s) {
         HashMap <Character, Integer> map = new HashMap<>();
-        int count = 0, hasOdd = 0;
+        int count = 0;
+        boolean hasOdd = false;
         for(int  i=0; i<s.length(); i++) {
             if(map.containsKey(s.charAt(i))) {
                 map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
@@ -15,10 +16,10 @@ class Solution {
                 count += val;
             } else {
                 count += (val-1);
-                hasOdd = 1;
+                hasOdd = true;
             }
         }
-        if(hasOdd == 1)  {
+        if(hasOdd)  {
             count++;
         }
         return count;
