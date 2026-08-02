01a579bce3aca5dd4e12e7fa09dc0c6ff8c301e8
Himangsu Sen <himangsusen18@gmail.com>
2026-08-02 12:23:01 +0530

    Time: 35 ms (68.21%), Space: 48.2 MB (52.59%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,10 +1,8 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
-
 class Solution {
     public String removeStars(String s) {
         Deque<Character> st = new ArrayDeque<>();
-
         for (char c : s.toCharArray()) {
             if (c == '*') {
                 st.pop();
@@ -12,13 +10,10 @@ class Solution {
                 st.push(c);
             }
         }
-
         StringBuilder res = new StringBuilder();
-
         while (!st.isEmpty()) {
             res.append(st.pop());
         }
-
         return res.reverse().toString();
     }
 }
