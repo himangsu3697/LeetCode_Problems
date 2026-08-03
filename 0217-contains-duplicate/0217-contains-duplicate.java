f8b7506387f1517a61385afb230e1a8898585875
Himangsu Sen <himangsusen18@gmail.com>
2026-08-03 20:03:13 +0530

    Time: 23 ms (30.83%), Space: 92.9 MB (76.52%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,10 @@
-import java.util.HashMap;
+import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap <Integer, Integer> map = new HashMap<>();
+        HashSet <Integer> set = new HashSet<>();
         for(int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+            set.add(n);
         }
-        for(int v : map.values()) {
-            if(v >= 2) {
-                return true;
-            }
-        }
-        return false;
+        return (set.size() == nums.length) ? false : true;
     }
 }
