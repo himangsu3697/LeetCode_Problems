7419dd612a8e099d797cb8c02988c24fccf64e27
Himangsu Sen <himangsusen18@gmail.com>
2026-08-10 07:44:15 +0530

    Time: 2 ms (94.02%), Space: 71.9 MB (53.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -19,3 +19,29 @@ class Solution {
         return nums;
     }
 }
+
+/* 
+preix and ssuffix solution
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        // Prefix product
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Suffix product
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return ans;
+    }
+}
+*/
