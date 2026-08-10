9505b66e9b7314d63e197f5131e671480436c4f0
Himangsu Sen <himangsusen18@gmail.com>
2026-08-10 07:17:46 +0530

    Time: 2 ms (94.02%), Space: 71.7 MB (61.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int res[] = new int[nums.length];
         int product = 1;
         int count = 0; 
         for(int i=0; i<nums.length; i++) {
@@ -12,11 +11,11 @@ class Solution {
         }
         for(int i=0; i<nums.length; i++) {
             if(nums[i] == 0) {
-                res[i] = (count > 1) ? 0 :  product;
+                nums[i] = (count > 1) ? 0 :  product;
             } else {
-                res[i] = (count > 0) ? 0 : product/nums[i];
+                nums[i] = (count > 0) ? 0 : product/nums[i];
             }
         } 
-        return res;
+        return nums;
     }
 }
