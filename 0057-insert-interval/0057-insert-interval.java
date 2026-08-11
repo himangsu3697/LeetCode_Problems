9fe50f76f6900e6444a17ac9bf6b0d7f0da878bb
Himangsu Sen <himangsusen18@gmail.com>
2026-08-11 21:24:55 +0530

    Time: 1 ms (98.13%), Space: 47.3 MB (20.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -8,8 +8,12 @@ class Solution {
         }
 
         while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            if (intervals[i][0] < newInterval[0])
+                newInterval[0] = intervals[i][0];
+    
+            if (intervals[i][1] > newInterval[1])
+                newInterval[1] = intervals[i][1];
+
             i++;
         }
         res[idx++] = newInterval;
