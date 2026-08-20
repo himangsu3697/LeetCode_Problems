f5140211f06adfa3eb1544e35598edb1975cb74c
Himangsu Sen <himangsusen18@gmail.com>
2026-08-20 08:06:07 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.65%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,21 +1,13 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length < 3) {
-            if (nums.length == 2 && nums[0] < nums[1]) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        int i = 1;
-        for (; i < nums.length - 1; i++) {
-            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 return i;
             }
         }
-        if (nums[i - 1] < nums[i] && i == nums.length - 1) {
-            return i;
-        }
-        return 0;
+
+        return n - 1;
     }
 }
