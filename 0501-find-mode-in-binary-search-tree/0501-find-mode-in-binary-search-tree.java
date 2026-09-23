e0bd669c2d3b8e461233714b6abadaf29f598034
Himangsu Sen <himangsusen18@gmail.com>
2026-09-23 19:27:42 +0530

    Time: 0 ms (100%), Space: 46.9 MB (61.47%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -1,46 +1,46 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- import java.util.HashMap;
+import java.util.*;
+
 class Solution {
-    public void helper(TreeNode root, HashMap<Integer, Integer> map) {
-        if(root != null) {
-            helper(root.left, map);
-            map.put(root.val, map.getOrDefault(root.val, 0) + 1);
-            helper(root.right, map);
-        }
-    }
+    List<Integer> result = new ArrayList<>();
+    int maxCount = 0;
+    int count = 0;
+    Integer prev = null;
 
     public int[] findMode(TreeNode root) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        helper(root, map);
-        int max[] = new int[2];
-        int res[] = new int[map.size()];
-        int idx = 0;
-        for(int key : map.keySet()) {
-            if(max[1] < map.get(key)) {
-                max[0] = key; 
-                max[1] = map.get(key);
-            }
+        inorder(root);
+
+        int[] ans = new int[result.size()];
+
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
         }
-        
-        for(int key : map.keySet()) {
-            if(max[1] == map.get(key)) {
-                res[idx++] = key;
-            }
+
+        return ans;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
         }
-        return Arrays.copyOf(res, idx);
+
+        inorder(root.left);
+
+        if (prev == null || prev != root.val) {
+            count = 1;
+        } else {
+            count++;
+        }
+
+        if (count > maxCount) {
+            result.clear();
+            result.add(root.val);
+            maxCount = count;
+        } else if (count == maxCount) {
+            result.add(root.val);
+        }
+
+        prev = root.val;
+
+        inorder(root.right);
     }
 }
