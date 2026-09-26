5fef6765665c217d35426b95d80c77eee773ec11
Himangsu Sen <himangsusen18@gmail.com>
2026-09-26 20:10:26 +0530

    Time: 17 ms (75.82%), Space: 48.8 MB (39.61%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,40 +14,48 @@
  *     }
  * }
  */
- import java.util.ArrayList;
+import java.util.ArrayList;
+
 class Solution {
     public void helper(TreeNode root, List<Integer> list) {
-        if(root != null) {
+        if (root != null) {
             helper(root.left, list);
             list.add(root.val);
             helper(root.right, list);
         }
     }
+
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List <Integer> list1 = new ArrayList<>();
-        List <Integer> list2 = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
         helper(root1, list1);
         helper(root2, list2);
-        if(root1 == null) {
+        if (root1 == null) {
             return list2;
         }
-        if(root2 == null) {
+        if (root2 == null) {
             return list1;
         }
         int i = 0, j = 0;
-        while(i < list1.size() && j < list2.size()) {
-            if(list1.get(i) < list2.get(j)) {
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) < list2.get(j)) {
+                res.add(list1.get(i));
                 i++;
             } else {
-                list1.add(i, list2.get(j));
+                res.add(list2.get(j));
                 j++;
-                i++;
             }
         }
-        while(j < list2.size()) {
-            list1.add(list2.get(j++));
+        while (i < list1.size()) {
+            res.add(list1.get(i));
+            i++;
         }
-        
-        return list1;
+        while (j < list2.size()) {
+            res.add(list2.get(j));
+            j++;
+        }
+
+        return res;
     }
 }
